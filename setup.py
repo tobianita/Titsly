4d3e6eab4f2e743c9640f8c0efd1ce9a74254839
tobianita <tobianita4@gmail.com>
2025-12-16 00:22:25 +0100

    second corrected error in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
   from distutils.core import setup
 
 config = {
-  'name': 'Titsly'
-  'description' ; 'Titsly is a url shortening python server',
-  'author': 'Shadrach Tayo',
-  'url': 'titsly.com',
-  'author_email': 'shadrachtemitayo@gmail.com',
-  'version': '0.1',
-  'install_requires': ['nose'],
+  'name' : 'Titsly'
+  'description'='Titsly is a url shortening python server',
+  'author'='Shadrach Tayo',
+  'url'='titsly.com',
+  'author_email'='shadrachtemitayo@gmail.com',
+  'version'='0.1',
+  'install_requires' : ['nose'],
   'packages': [],
   'scripts': []
 }
