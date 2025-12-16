f2752dfcf40a040cf54191fcc8974378388db884
tobianita <tobianita4@gmail.com>
2025-12-16 01:06:23 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
   from distutils.core import setup
 
 config = {
-  'name' : 'Titsly',
-  description='Titsly is a url shortening python server',
-  'author'='Shadrach Tayo',
-  'url'='titsly.com',
-  'author_email'='shadrachtemitayo@gmail.com',
-  'version'='0.1',
-  'install_requires' : ['nose'],
-  'packages': [],
-  'scripts': []
+  # 'name' : 'Titsly',
+  # 'description': 'Titsly is a url shortening python server',
+  # 'author' : 'Shadrach Tayo',
+  # 'url' : 'titsly.com',
+  # 'author_email' : 'shadrachtemitayo@gmail.com',
+  # 'version' : '0.1',
+  # 'install_requires' : ['nose'],
+  # 'packages': [],
+  # 'scripts': [],
 }
 
 setup(**config)
